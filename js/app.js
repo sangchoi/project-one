cb7b4f571f8c1981b215202bc975cd7010eb5f6c
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-02-10 08:47:03 -0800

    Level 2 implemented

--- js/app.js
+++ js/app.js
@@ -1,57 +1,75 @@
 
-document.addEventListener("DOMContentLoaded", init);
+// document.addEventListener("DOMContentLoaded", getWord);
 
 //variables
-var correctWords = [];
+var correctWords1 = [];
+var correctWords2 = [];
 var enterButton;
 var matched;
 var newWord = 0;
 var timer = null;
+var gameLevel = 1;
 
 
 // Level One words
 var levelOne = ["cat", "hat", "mat", "bat", "that", "chat", "sat", "fat", "pat", "gnat"];
 var levelTwo = ["hot", "not", "shot", "cot", "dot", "got", "jot", "lot", "pot", "rot"];
 
+
 // Colors
 var colors = ["red", "blue", "yellow", "green", "purple", "orange"]
-// var colors = [redKaboom, blueKaboom, yellowKaboom, greenKaboom, purpleKaboom, orangeKaboom]
 
-// Kaboom color
-var randomNum = Math.floor(Math.random() * colors.length);
+
+// Kaboom random color
+var randomNum = Math.floor(Math.random() * colors.length)
 
 
 //DOM elements
-var enterButton = document.getElementById("button");
+var enterButton = document.getElementById("submitButton");
+var resetButton = document.getElementById("resetButton");
 var currentWord = document.getElementById("word");
 var inputWord = document.getElementById("input");
 var form1 = document.getElementById("form1");
-var wordCollection = document.getElementById("wordCollection");
-// var kaboom = document.getElementById("kaboom").style.color;
-// var redKaboom = document.getElementById("kaboom").style.color = "red";
-// var blueKaboom = document.getElementById("kaboom").style.color = "blue";
-// var yellowKaboom = document.getElementById("kaboom").style.color = "yellow";
-// var greenKaboom = document.getElementById("kaboom").style.color = "green";
-// var purpleKaboom = document.getElementById("kaboom").style.color = "purple";
-// var orangeKaboom = document.getElementById("kaboom").style.color = "orange";
+var form1Reset = document.getElementById("form1").reset();
+var wordCollection1 = document.getElementById("wordCollection1");
+var wordCollection2 = document.getElementById("wordCollection2");
+var kaboom = document.getElementById("kaboom");
 
 
-
-
-
-// Initializing - Page will load with word
-function init() {
+// Getting Word 
+function getWord(level) {
     // Get a random index number for word
-    var randomNumber = Math.floor(Math.random() * levelOne.length)
-    // Replace current word with a random word from the levelOne array
-    currentWord.textContent = levelOne[randomNumber];
-    // Remove current word from levelOne array once shown on screen 
-    levelOne.splice(randomNumber, 1);
-    // Set timer to five seconds if there are still words in the levelOne array
-    if (levelOne.length > 0) {
-        timer = setTimeout(init, 5000);
+    var randomNumber;
+
+    if (level === 1) {
+        randomNumber = Math.floor(Math.random() * levelOne.length)
+        // Replace current word with a random word from the levelOne array
+        currentWord.textContent = levelOne[randomNumber];
+        kaboom.style.color = colors[randomNum];
+        // Remove current word from levelOne array once shown on screen 
+        levelOne.splice(randomNumber, 1);
+        // Set timer to five seconds if there are still words in the levelOne array
+        if (levelOne.length > 0) {
+            timer = setTimeout(() => getWord(level), 5000);
+        } else {
+            gameLevel = 2;
+            getWord(gameLevel);
+        }
     } else {
-        secondLevel();
+        currentWord.style.color = "#5A5AFE";
+        randomNumber = Math.floor(Math.random() * levelTwo.length)
+        // Replace current word with a random word from the levelOne array
+        currentWord.textContent = levelTwo[randomNumber];
+        kaboom.style.color = colors[randomNum];
+        // Remove current word from levelOne array once shown on screen 
+        levelTwo.splice(randomNumber, 1);
+        // Set timer to five seconds if there are still words in the levelOne array
+        if (levelTwo.length > 0) {
+            timer = setTimeout(() => getWord(level), 3000);
+        } else {
+            currentWord.textContent = "Woohoo~ Let's try again!"
+            console.log("Game Over!");
+        }
     }
 }
 
@@ -62,17 +80,22 @@ form1.addEventListener("submit", function(e) {
     e.preventDefault();
     // Check if input value equals the current word
     if (inputWord.value === currentWord.textContent) {
+        // kaboom.style.color = colors[randomNum];
+
+        if (gameLevel === 1) {
+            // If it does, then push the word into the correctWords array
+            correctWords1.push(inputWord.value);
+            // The words in the array will then be collected and shown on the page
+            wordCollection1.textContent = "Level One: " + correctWords1;
+        } else {
+            correctWords2.push(inputWord.value);
+            wordCollection2.textContent = "Level Two: " + correctWords2;
+        }
         
-        document.getElementById("kaboom").style.color = colors[randomNum];
-        // If it does, then push the word into the correctWords array
-        correctWords.push(inputWord.value);
-        // The words in the array will then be collected and shown on the page
-        wordCollection.textContent = correctWords;
         // Clears input value once entered
         inputWord.value = "";
         clearTimeout(timer);
-        init();
-        
+        getWord(gameLevel);
         console.log("You have a match!")
         console.log(correctWords);
         // return true;
@@ -82,10 +105,17 @@ form1.addEventListener("submit", function(e) {
         inputWord.value = "";
         // timer();
         clearTimeout(timer);
-        init();
+        getWord(gameLevel);
         // return false;
     }
 });
+
+form1.addEventListener("reset", function() {
+    resetButton = form1Reset;
+})
+
+getWord(gameLevel);
+
 
 ////////////////// secondLevel ////////////////////////
 
@@ -95,34 +125,38 @@ form1.addEventListener("submit", function(e) {
 //         var randomNumber = Math.floor(Math.random() * levelTwo.length)
 //         // Replace current word with a random word from the levelTwo array
 //         currentWord.textContent = levelTwo[randomNumber];
+//         currentWord.style.color = "#5A5AFE";
 //         // Remove current word from levelTwo array once shown on screen 
 //         levelTwo.splice(randomNumber, 1);
 //         // Set timer to five seconds if there are still words in the levelTwo array
 //         if (levelTwo.length > 0) {
-//         timer = setTimeout(secondLevel, 5000);
-//     } else {
+//         timer = setTimeout(secondLevel, 3000);
+//         } else {
 //         currentWord.textContent = "Let's try again!";
 //     }
 // }
 // }
 
+
+// if (levelOne.length === 0 && correctWords.length >= 8) {
 // form1.addEventListener("submit", function(e) {
 //     console.log(currentWord, inputWord.value);
 //     // console.log({currentWord, input: inputWord.value});
 //     e.preventDefault();
 //     // Check if input value equals the current word
 //     if (inputWord.value === currentWord.textContent) {
-//         document.getElementById("kaboom").style.color = colors[randomNum];
+//         kaboom = colors[randomNum];
 //         // If it does, then push the word into the correctWords array
-//         correctWords.push(inputWord.value);
+//         correctWords2.push(inputWord.value);
+//         console.log("wordcollection2:", wordCollection2);
 //         // The words in the array will then be collected and shown on the page
-//         wordCollection.textContent = correctWords;
+//         wordCollection2.textContent = "Level Two: " + correctWords2;
 //         // Clears input value once entered
 //         inputWord.value = "";
 //         clearTimeout(timer);
 //         secondLevel();
 //         console.log("You have a match!")
-//         console.log(correctWords);
+//         console.log(correctWords2);
 //     } else {
 //         console.log("Incorrect");
 //         // Clears input value once entered
@@ -132,5 +166,5 @@ form1.addEventListener("submit", function(e) {
 //         secondLevel();
 //     }
 // });
-
+// }
 
